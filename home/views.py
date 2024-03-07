b6c296dcd7f4d0c33b47b1e2a2912db44a77793d
DnA-TUANANGUYEN <dna-tuananguyen@DnA-TUANANGUYENs-MacBook-Air.local>
2024-03-07 22:30:34 +0700

    update

---
+++ home/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+
+
+"""
+To render html web pages
+"""
+HTML_STRING = """
+<h1> This is Home Page </h1>
+"""
+
+
+def home(request):
+    """
+    Take in a request (Django sends request)
+    Return HTML as a response
+    """
+    return HttpResponse(HTML_STRING)
