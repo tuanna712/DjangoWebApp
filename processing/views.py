9b3576809ca11fefbf91e0788ffb5db332ced3e3
DnA-TUANANGUYEN <dna-tuananguyen@DnA-TUANANGUYENs-MacBook-Air.local>
2024-03-12 22:45:24 +0700

    registration

--- processing/views.py
+++ processing/views.py
@@ -24,3 +24,6 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out')
     return redirect('home')
+
+def register_user(request):
+    return render(request, 'register.html', {})
