85aed00609ebfd937b2812381f337da1bc1baf59
DnA-TUANANGUYEN <dna-tuananguyen@DnA-TUANANGUYENs-MacBook-Air.local>
2024-03-13 01:04:33 +0700

    add and update record

--- processing/forms.py
+++ processing/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
@@ -29,3 +30,18 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
 
+
+# Create Add Record Form
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}), label="")
+    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}), label="")
+    address = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address'}), label="")
+    email = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email'}), label="")
+    phone = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Phone'}), label="")
+    city = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'City'}), label="")
+    state = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'State'}), label="")
+    zipcode = forms.CharField(required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Zipcode'}), label="")
+
+    class Meta:
+        model = Record
+        exclude = ("user", )
